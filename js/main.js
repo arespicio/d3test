6dd89bd238c3afe0403c7b6dd12cbd6d27dab130
Ashleigh Respicio <ashleighrespicio@gmail.com>
2014-08-05 21:25:21 -0400

    added legend need to link json file

--- js/main.js
+++ js/main.js
@@ -1,41 +1,106 @@
-$.getJSON("/data/revenue.json", function(data) {
-    console.log(data);
-    piechart(data);
-});
+$.getJSON("/data/revenue.json") 
+    .done(function(data) {
+    // console.log(data);
 
-function piechart(data) {
-    var w = 750; //width
-    var h = 600; //height
-    var r = 300; //radius
-    var color = d3.scale.category20c(); //builtin range of colors
+    var WIDTH = 600, HEIGHT = 450;
 
-    var vis = d3.select("#container")
-        .append("svg:svg") //create the SVG element inside the <body>
+    var name = "name";
+    var data = "revenue";
+
+    var w = WIDTH,
+        h = HEIGHT, 
+        r = Math.min(w, h) / 3;
+
+    var COLOR_1 = "#98abc5";
+    var COLOR_2 = "#8a89a6";
+    var COLOR_3 = "#7b6888";
+    var COLOR_4 = "#6b486b";
+    var COLOR_5 = "#a05d56";
+
+    var color = d3.scale.ordinal()
+        .range([COLOR_1, COLOR_2, COLOR_3,
+                COLOR_4, COLOR_5]);
+
+    var arc = d3.svg.arc()
+        .innerRadius(0)
+        .outerRadius(r - 10);
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d[data]; });
+
+
+    var svg = d3.select("#container")
+        .append("svg") //create the SVG element inside the <body>
         .data([data]) //associate our data with the document
         .attr("width", w)
         .attr("height", h)
-        .append("svg:g")
-        .attr("transform", "translate(" + r + "," + r + ")");
+        .append("g")
+        .attr("transform", "translate(" + w / 3 + "," + h / 2 + ")");
 
-    var arc = d3.svg.arc()
-        .outerRadius(r);
+    var drawD3Document = function(data) {
+        data.forEach(function(d) {
+            d[data] = +d[data];
+    });
+    var g = svg.selectAll(".arc")
+    	    .data(pie(data))
+    	    .enter()
+    		.append("g")
+    		.attr("class", "arc");
 
-    var pie = d3.layout.pie()
-        .value(function(d) {return d.revenue; });
+    var count = 0;
 
-    var arcs = vis.selectAll("g.slice")
-    	.data(pie)
-    	.enter()
-    		.append("svg:g")
-    			.attr("class", "slice");
+    	g.append("path")
+            .attr("d", arc)
+    		.attr("id", function(d) { return "arc-" + (count++); })
+            .style("fill", function(d) {
+                return color(d.data[name]);
+            });
 
-    	arcs.append("svg:path")
-    		.attr("fill", function(d, i) {return color(i); } )
-    		.attr("d", arc);
-}
+        g.append("text")
+            .attr("transform", function(d) {
+                return "translate(" + arc.centroid(d) + ")";
+            })
+            .attr("dy", ".35em").style("text-anchor", "middle")
+            .text(function(d) {
+                return d.data[name];
+            });
+
+        count = 0;
+        var legend = svg.selectAll(".legend")
+            .data(data).enter()
+            .append("g").attr("class", "legend")
+            .attr("legend-id", function(d) {
+                return count++;
+            })
+            .attr("transform", function(d, i) {
+                return "translate(-60," + (-70 + i * 20) + ")";
+            })
+            .on("click", function() {
+                  console.log("#arc-" + $(this).attr("legend-id"));
+                  var arc = d3.select("#arc-" + $(this).attr("legend-id"));
+                  arc.style("opacity", 0.3);
+                  setTimeout(function() {
+                      arc.style("opacity", 1);
+                  }, 1000);
+        });
+
+    legend.append("rect")
+        .attr("x", w / 2)
+        .attr("width", 18).attr("height", 18)
+        .style("fill", function(d) {
+            return color(d[name]);
+        });
+    legend.append("text").attr("x", w / 2)
+        .attr("y", 9).attr("dy", ".32em")
+        .style("text-anchor", "end").text(function(d) {
+          return d[name];
+        });  
+
+};
 
 
-//create function that add key value pair of color and name from data
+//create function that adds key value pair of color and name from data
 //color(i) 
 //make loop that goes through each data item 
 //inside sidebar create html element with text of name of data and color from color(i)
